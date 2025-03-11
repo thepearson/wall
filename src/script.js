84ad4dc217bef7f6d89d31875a750334828cb92e
Craig Pearson <thepearson@gmail.com>
2025-03-12 06:22:44 +1300

    Updated controls

--- src/script.js
+++ src/script.js
@@ -28,16 +28,17 @@ const scene = new THREE.Scene()
 
 const parameters = {};
 parameters.speed = 0.0001;
-parameters.clear = '#AAAAFF';
-parameters.items_color = '#FFFFFF';
-parameters.items_x = 80;
-parameters.items_y = 40;
+parameters.clear = new THREE.Color('#AAAAFF');
+parameters.items_color = new THREE.Color('#FFFFFF');
+parameters.items_x =10;
+parameters.items_y = 10;
 parameters.pulse_enabled = false;
 parameters.pulse_value = 0.1;
 parameters.noise_scale = 1.0;
-parameters.fog_color = '#Fbf3ff';
+parameters.noise_modifier = 0.5;
+parameters.fog_color = new THREE.Color('#Fbf3ff');
 parameters.fog_start = 40;
-parameters.fog_end = 60;
+parameters.fog_end = 80;
 parameters.geometry_type = 'box';
 parameters.geometry_size = 1.0;
 parameters.camera_perspective = 55;
@@ -45,7 +46,7 @@ parameters.camera_position_z = 10;
 parameters.camera_position_y = 2;
 parameters.camera_position_x = -3;
 parameters.camera_rotation_y = 0.3;
-parameters.lights_sun_color = '#FFFFFF';
+parameters.lights_sun_color = new THREE.Color('#FFFFFF');
 parameters.lights_sun_intensity = 2;
 
 
@@ -53,15 +54,15 @@ const cubes = [];
 
 const generateCubes = () => {
 
-
-    for (let cube of cubes) {
-        scene.remove(cube);
-    }
+    scene.remove.apply(scene, scene.children);
+    // for (let cube of cubes) {
+    //     scene.remove(cube);
+    // }
     
     // Fog (color, near, far)
-    scene.fog = new THREE.Fog(new THREE.Color(parameters.fog_color), parameters.fog_start, parameters.fog_end);
+    scene.fog = new THREE.Fog(parameters.fog_color, parameters.fog_start, parameters.fog_end);
 
-    const light = new THREE.DirectionalLight(new THREE.Color(parameters.lights_sun_color), parameters.lights_sun_intensity);
+    const light = new THREE.DirectionalLight(parameters.lights_sun_color, parameters.lights_sun_intensity);
     light.position.set(parameters.camera_position_x, parameters.camera_position_y, parameters.camera_position_z + 10);
     light.castShadow = true;
     light.shadow.mapSize.width = 1024;
@@ -75,7 +76,7 @@ const generateCubes = () => {
     scene.add(light);
 
     // Ambient light (color, intensity)
-    const ambientLight = new THREE.AmbientLight(0xaaaaff, 0.5); // Blueish white
+    const ambientLight = new THREE.AmbientLight(0xff00ff, 0.5); // Blueish white
     scene.add(ambientLight);
 
     const gridSizeX = parameters.items_x;
@@ -88,7 +89,7 @@ const generateCubes = () => {
         geometry = new THREE.BoxGeometry(parameters.geometry_size, parameters.geometry_size, parameters.geometry_size);
     }
 
-    const material = new THREE.MeshPhongMaterial({ color: new THREE.Color(parameters.items_color) });
+    const material = new THREE.MeshPhongMaterial({ color: parameters.items_color });
 
     for (let x = 0; x < gridSizeX; x++) {
         for (let y = 0; y < gridSizeY; y++) {
@@ -118,7 +119,7 @@ const update = (elapsedTime) => {
             cube.scale.set(1 + pulsation, 1 + pulsation, 1 + pulsation);                    
         }
 
-        cube.position.z = noiseValue * 0.5 ;
+        cube.position.z = noiseValue * parameters.noise_modifier;
     });
 
 }
@@ -147,10 +148,16 @@ parameters.lights_sun_color = 0xFFFFFF;
 parameters.lights_sun_intensity = 20;
 */
 gui.add(parameters, 'speed').min(0.0001).max(0.001).step(0.0001)
-//gui.add(parameters, 'clear').onFinishChange(generateCubes)
-//gui.add(parameters, 'items_x').min(10).max(100).step(5).onFinishChange(generateCubes)
-//gui.add(parameters, 'items_y').min(10).max(100).step(5).onFinishChange(generateCubes)
+gui.addColor(parameters, 'clear').onChange(() => renderer.setClearColor(new THREE.Color(parameters.clear)))
+gui.add(parameters, 'items_x').min(10).max(100).step(5).onChange(generateCubes)
+gui.add(parameters, 'items_y').min(10).max(100).step(5).onChange(generateCubes)
 gui.add(parameters, 'noise_scale').min(0.01).max(2).step(0.01)
+gui.add(parameters, 'noise_modifier').min(0.5).max(2).step(0.25)
+
+gui.addColor(parameters, 'fog_color').onChange(generateCubes)
+gui.add(parameters, 'fog_start').min(0).max(100).step(1).onChange(generateCubes)
+gui.add(parameters, 'fog_end').min(0).max(100).step(1).onChange(generateCubes)
+
 //gui.add(parameters, 'geometry_size').min(0.1).max(3).step(0.1).onFinishChange(generateCubes)
 
 // gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
